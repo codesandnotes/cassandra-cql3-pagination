7b4b10d9c0bcf991429952f4d0b0ed0076c3a66d
Diego Pappalardo <codesandnotes>
2015-10-01 09:54:15 +0200

    Modified compund PK example to make it more representative.

--- src/test/java/codesandnotes/cql3pagination/CompoundPrimaryKeyTest.java
+++ src/test/java/codesandnotes/cql3pagination/CompoundPrimaryKeyTest.java
@@ -3,7 +3,6 @@ package codesandnotes.cql3pagination;
 import codesandnotes.Integration;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
-import com.datastax.driver.core.querybuilder.Select;
 import com.datastax.driver.core.querybuilder.TokenClause;
 import org.junit.After;
 import org.junit.Assert;
@@ -27,23 +26,28 @@ public class CompoundPrimaryKeyTest extends Integration {
     }
 
     @Test
+    public void wtf() {
+        print("", embeddedCassandra.session().execute("select * from cql3_pagination.compound_pk").all());
+    }
+
+    @Test
     public void retrieve_5_ResultsOfPage_2_InAListOf_10_UsingCQL() {
         List<Row> page1 = embeddedCassandra.session().execute(
                 QueryBuilder.select().from(CASSANDRA_KEYSPACE, TABLE).limit(5)
         ).all();
         print("page 1:", page1);
 
-        String lastCountryInPage1 = page1.get(4).getString("country");
-        String lastUserInPage1 = page1.get(4).getString("user");
+        String lastBookInPage1 = page1.get(4).getString("book");
+        int lastLineInPage1 = page1.get(4).getInt("line");
 
         List<Row> page2 = embeddedCassandra.session().execute(
-                "select * from cql3_pagination.compound_pk where token(country) >= token('" + lastCountryInPage1
-                        + "') and user > '" + lastUserInPage1 + "' limit 5 allow filtering"
+                "select * from cql3_pagination.compound_pk where token(book) >= token('" + lastBookInPage1
+                        + "') and line > " + lastLineInPage1 + " limit 5 allow filtering"
         ).all();
         print("page 2:", page2);
 
         Assert.assertEquals(5, page2.size());
-        Assert.assertNotEquals(page2.get(0).getString("country"), page1.get(0).getString("user"));
+        Assert.assertNotEquals(page1.get(0).getString("content"), page2.get(0).getString("content"));
     }
 
     @Test
@@ -53,24 +57,24 @@ public class CompoundPrimaryKeyTest extends Integration {
         ).all();
         print("page 1:", page1);
 
-        String lastCountryInPage1 = page1.get(4).getString("country");
-        String lastUserInPage1 = page1.get(4).getString("user");
+        String lastBookInPage1 = page1.get(4).getString("book");
+        int lastLineInPage1 = page1.get(4).getInt("line");
 
         List<Row> page2 = embeddedCassandra.session().execute(
                 QueryBuilder.select().from(CASSANDRA_KEYSPACE, TABLE).allowFiltering()
-                        .where(new TokenClause("country", ">=", lastCountryInPage1))
-                        .and(QueryBuilder.gt("user", lastUserInPage1)).limit(5)
+                        .where(new TokenClause("book", ">=", lastBookInPage1))
+                        .and(QueryBuilder.gt("line", lastLineInPage1)).limit(5)
         ).all();
         print("page 2:", page2);
 
         Assert.assertEquals(5, page2.size());
-        Assert.assertNotEquals(page2.get(0).getString("country"), page1.get(0).getString("user"));
+        Assert.assertNotEquals(page1.get(0).getString("content"), page2.get(0).getString("content"));
     }
 
     private void print(String header, List<Row> rows) {
         System.out.println(header);
         for (Row row : rows) {
-            System.out.println(row.getString("country") + " | " + row.getString("user"));
+            System.out.println(row.getString("book") + " | " + row.getInt("line") + " | " + row.getString("content"));
         }
     }
 }
